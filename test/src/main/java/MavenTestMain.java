afe8364a6e438bdfddd16a16aa5214aec454d39c
lsqcode <2665528834@qq.com>
2021-06-04 10:51:00 +0800

    开发分支冲突

--- test/src/main/java/MavenTestMain.java
+++ test/src/main/java/MavenTestMain.java
@@ -3,7 +3,7 @@ public class MavenTestMain {
     public static String outputTest(String str)
     {
         System.out.println("str");
-        System.out.println("测试");
+        System.out.println("测试123456");
         return str;
     }
 }
