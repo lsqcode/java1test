06eee66dec2f8b91695a92a5b4870d28669db904
lsqcode <2665528834@qq.com>
2021-06-04 10:46:43 +0800

    zai添加一行测试

--- test/src/main/java/MavenTestMain.java
+++ test/src/main/java/MavenTestMain.java
@@ -5,6 +5,7 @@ public class MavenTestMain {
         System.out.println("str");
         System.out.println("测试");
         System.out.println();
+        System.out.println();
         return str;
     }
 }
