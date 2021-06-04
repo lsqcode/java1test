5b40d226cbea28b62155bf6ab19370966474bd69
lsqcode <2665528834@qq.com>
2021-06-04 11:16:33 +0800

    添加一行

--- test/src/main/java/MavenTestMain.java
+++ test/src/main/java/MavenTestMain.java
@@ -6,6 +6,7 @@ public class MavenTestMain {
         System.out.println("测试111");
         System.out.println();
         System.out.println("开发分支测试");
+        System.out.println("添加一行");
         return str;
     }
 }
