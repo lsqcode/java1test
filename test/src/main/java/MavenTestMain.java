b4c771122b8ed03598e81f28bdc06fd205da4f8e
lsqcode <2665528834@qq.com>
2021-06-04 10:33:11 +0800

    再次提交测试

--- test/src/main/java/MavenTestMain.java
+++ test/src/main/java/MavenTestMain.java
@@ -3,6 +3,7 @@ public class MavenTestMain {
     public static String outputTest(String str)
     {
         System.out.println("str");
+        System.out.println("测试");
         return str;
     }
 }
